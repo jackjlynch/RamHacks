b51ed94ce57d5b60a85e1aaf6bf441d781160906
Jack Lynch <jacklynch@wharnal.com>
2014-11-08 17:12:33 -0500

    fleshed out some of language_stats, added model for dictionaries

--- foreignArticleFinder/models.py
+++ foreignArticleFinder/models.py
@@ -4,6 +4,7 @@ class Language(models.Model):
     languageName = models.TextField()
     latinAlphabet = models.BooleanField(default=True)
     wordList = models.TextField()
+    articlesSources = models.TextField()
     updateDate = models.DateTimeField('date last updated')
 
     def __str__(self):
@@ -14,3 +15,9 @@ class Article(models.Model):
     language = models.ForeignKey(Language)
     source = models.TextField()
     text = models.TextField()
+
+
+class WordList(models.Model):
+    ordered = models.BooleanField(default=True)
+    words = models.TextField()
+    separator = models.TextField(default=" ")
