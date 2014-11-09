78cfea564afb7e0946a4cc49594388b744e85a5c
Jack Lynch <jacklynch@wharnal.com>
2014-11-09 02:14:14 -0500

    index grabs articles with ajax now

--- foreignArticleFinder/crawler.py
+++ foreignArticleFinder/crawler.py
@@ -10,4 +10,4 @@ class Crawler():
         if self.source.type == "rss":
             for article in  feedparser.parse(self.source.url)["entries"]:
                 if not Article.objects.filter(title=article['title']).exists():
-                    Article(language=self.source.language, source=self.source, text=article['summary_detail'], title=article['title'], author=article['author'], url=article['link'], pub_date=article['published']).save()
+                    Article(language=self.source.language, source=self.source, text=article['summary_detail']['value'], title=article['title'], author=article['author'], url=article['link'], pub_date=article['published']).save()
--- foreignArticleFinder/views.py
+++ foreignArticleFinder/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from foreignArticleFinder.models import Article, Source
 from foreignArticleFinder.crawler import Crawler
+from django.core import serializers
+import json
 
 
 # Create your views here.
@@ -10,11 +12,15 @@ def index(request):
 
 
 def article(request, article_id=-1):
-    return render(request, "foreignArticleFinder/article.html", {"article": get_object_or_404(Article, pk=article_id)})
+    #return render(request, "foreignArticleFinder/article.html", {"article": get_object_or_404(Article, pk=article_id)})
+
+    #magic
+    return HttpResponse(json.dumps(json.loads(serializers.serialize("json", [get_object_or_404(Article, pk=article_id), ], fields = ("text", "author", "pub_date")))[0]))
 
 
 def articles(request):
-    return render(request, "foreignArticleFinder/articles.html", {"articles": Article.objects.all()})
+    #return render(request, "foreignArticleFinder/articles.html", {"articles": Article.objects.all()})
+    return HttpResponse(serializers.serialize("json", Article.objects.all(), fields = ("title")))
 
 
 def sources(request):
