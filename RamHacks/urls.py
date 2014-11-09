751caba7e7068848832be80bac2b7a44eb052af9
Jack Lynch <jacklynch@wharnal.com>
2014-11-09 13:54:47 -0500

    static files maybe

--- RamHacks/urls.py
+++ RamHacks/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from RamHacks import settings
 
 urlpatterns = patterns('',
     # Examples:
@@ -9,3 +10,9 @@ urlpatterns = patterns('',
     url(r'^finder/', include('foreignArticleFinder.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
+
+
+#static files
+urlpatterns += patterns('',
+        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+    )
