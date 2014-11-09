147508531ba943148f9e51f6223ed3eaf6fc85fb
Jack Lynch <jacklynch@wharnal.com>
2014-11-09 13:05:27 -0500

    maybe now it's actually ready for heroku

--- RamHacks/wsgi.py
+++ RamHacks/wsgi.py
@@ -12,3 +12,10 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "RamHacks.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
+
+
+#heroku serving static files
+from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
+
+application = Cling(get_wsgi_application())
