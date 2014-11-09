798538e4fd336c82f2443c483ad3e191f82a6b95
Jack Lynch <jacklynch@wharnal.com>
2014-11-09 13:40:00 -0500

    Revert "maybe now it's ready for heroku pt 8"
    This reverts commit 3030e2cda77593b9749c26a03eef10f5144fcb32.

--- RamHacks/settings.py
+++ RamHacks/settings.py
@@ -122,17 +122,3 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.7/howto/static-files/
-
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = 'staticfiles'
-STATIC_URL = '/static/'
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
