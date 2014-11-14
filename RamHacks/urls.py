ea17fef1c024854f842c41e88d3f83b883c4f1c7
Jack Lynch <jacklynch@wharnal.com>
2014-11-14 13:02:45 -0500

    root of site should redirect to app now

--- RamHacks/urls.py
+++ RamHacks/urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from RamHacks import settings
+from foreignArticleFinder import views
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'RamHacks.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', views.index, name='index'),
     url(r'^finder/', include('foreignArticleFinder.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
