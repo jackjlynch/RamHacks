d76c255ef570a0d4d30af46e9937e4c3fe540506
Jack Lynch <jacklynch@wharnal.com>
2015-10-28 12:09:48 -0400

    reformatted code

--- foreignArticleFinder/crawler.py
+++ foreignArticleFinder/crawler.py
@@ -12,7 +12,9 @@ class Crawler():
         if self.source.type == "rss":
             for article in  feedparser.parse(self.source.url)["entries"]:
                 if not Article.objects.filter(title=article['title']).exists():
-                    article = Article(language=self.source.language, source=self.source, text=article['summary_detail']['value'], title=article['title'], author=article['author'], url=article['link'], pub_date=article['published'])
+                    article = Article(language=self.source.language, source=self.source,
+                                      text=article['summary_detail']['value'], title=article['title'],
+                                      author=article['author'], url=article['link'], pub_date=article['published'])
                     article.wordsIn500 = reader.get_stats(article, 500)
                     article.wordsIn1000 = reader.get_stats(article, 1000)
                     article.wordsIn1500 = reader.get_stats(article, 1500)
